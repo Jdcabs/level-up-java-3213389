ddf21f3e04f0693b9cc0334ae599d30e13a0f458
Johndavid S Cabal <Johndavidcabal000@gmail.com>
2023-01-19 04:25:12 +0000

    modified changes

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,28 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+
+        if (password.length() < 6) {
+            return false;
+        }
+
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasNumber = false;
+
+        for (int i = 0; i < password.length() || !hasUpperCase || !hasLowerCase
+                || !hasNumber; i++) {
+            char currentDigit = password.charAt(i);
+
+            if (Character.isDigit(currentDigit)) {
+                hasNumber = true;
+            } else if (Character.isUpperCase(currentDigit)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(currentDigit)) {
+                hasLowerCase = true;
+            }
+        }
+        return hasUpperCase && hasLowerCase && hasNumber;
     }
 
     public static void main(String[] args) {
